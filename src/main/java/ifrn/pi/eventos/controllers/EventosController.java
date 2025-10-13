a0bd348316ae8f7a3a54c8f2ec7aca25b0f888ed
Pedro Henrique Felipe Felix <20221204010030@ifrn.local>
2025-10-13 13:56:56 -0300

    Enviando dados do formulario para a aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -2,6 +2,9 @@ package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
@@ -11,4 +14,15 @@ public class EventosController {
 		return "formEvento";
 	}
 
+	@RequestMapping("/eventos")
+	public String adicionar(Evento evento) {
+		
+		
+		
+		System.out.println(evento);
+		
+		
+		
+		return "evento-adicionar";
+	}
 }
