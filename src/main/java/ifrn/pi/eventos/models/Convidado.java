221dd6dd57e62a129e275f955ac4328907d3897f
Pedro Henrique Felipe Felix <20221204010030@ifrn.local>
2025-10-21 17:26:31 -0300

    cadastrando e listando os convidados do evento

---
+++ src/main/java/ifrn/pi/eventos/models/Convidado.java
@@ -0,0 +1,59 @@
+package ifrn.pi.eventos.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Convidado {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String nome;
+	private String rg;
+	
+	@ManyToOne
+	private Evento evento;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public Evento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+
+	@Override
+	public String toString() {
+		return "Convidado [Id=" + Id + ", nome=" + nome + ", rg=" + rg + ", evento=" + evento + "]";
+	}
+	
+	
+}
